34ac6698f1d735e7c8fd6e4c4db04f8627a0d5d3
Marvelous-Maddie <65891259+Marvelous-Maddie@users.noreply.github.com>
2020-07-02 16:44:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container mt-4">
+        <h1 className="display-4 text-center">
+          <i className="bi bi-list-check text-success" />
+          My<span className="text-success">ToDo</span>List
+        </h1>
+      </div>
+    </div>
+  );
+}
