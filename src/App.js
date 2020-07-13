e39fce1341bb1232a99b5032954f4b2afbdc4765
Madlen Engel <engel.madlen@googlemail.com>
2020-07-13 17:27:03 +0200

    Update structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Form from "./components/Form";
 import Table from "./components/Table";
+
 import "./styles.css";
 
 export default function App() {
@@ -27,17 +28,15 @@ export default function App() {
     <div className="App">
       <div className="container mt-4">
         <h1 className="display-4 text-center">
-          <i className="bi bi-list-check"></i>
           My<span className="text-success">ToDo</span>List
         </h1>
         <Form addTodo={addTodo} />
-        {todos.map((todo, index) => (
-          <Table key={index}
-            index={index}
-            todo={todo}
-            completeTodo={completeTodo}
-            removeTodo={removeTodo}/>
-          ))}
+        <Table key={index}
+          index={index}
+          todos={todos}
+          todo={todo}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo} />
       </div>
     </div>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,7 @@
 import React from "react";
+import TableBody from "./components/TableBody";
 
-const Table = ({ todo, index, completeTodo, removeTodo }) => {
+const Table = ({ todos, todo, index, completeTodo, removeTodo }) => {
   return(
     <table className="table table-striped mt-5">
       <thead>
@@ -9,19 +10,13 @@ const Table = ({ todo, index, completeTodo, removeTodo }) => {
           <th className="col-3"></th>
         </tr>
       </thead>
-      <tbody>
-        <tr className="d-flex">
-          <td className="col-9">{todo.text}</td>
-          <td className="col-3">
-            <button className="btn btn-success">
-              <i className="bi bi-check-square" onClick={() => completeTodo(index)}></i>
-            </button>
-            <button className="btn btn-danger">
-              <i className="bi bi-trash" onClick={() => removeTodo(index)}></i>
-            </button>
-          </td>
-        </tr>
-      </tbody>
+      {todos.map((todo, index) => (
+        <TableBody key={index}
+          index={index}
+          todo={todo}
+          completeTodo={completeTodo}
+          removeTodo={removeTodo}/>
+        ))}
     </table>
   )
 }
